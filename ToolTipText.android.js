6acef2d6c00ea286af857f80ae62c4fc343ffc90
Rocky Chan <rocky@oursky.com>
2016-04-27 21:31:06 +0800

    fix error on android

--- ToolTipText.android.js
+++ ToolTipText.android.js
@@ -1,11 +1,6 @@
 'use strict';
 
-var warning = require('warning');
-
 var ToolTipText = {
-  test: function() {
-    warning("Not yet implemented for Android.");
-  }
 };
 
 module.exports = ToolTipText;
